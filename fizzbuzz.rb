ccadb29026950dfd94df5353b8c7402f9216a533
AleshaRP <createlele@yahoo.com>
2018-06-19 19:43:53 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizz_buzz(int)
+  if int%15 == 0
+    puts "FizzBuzz"
+  elsif int%5 == 0
+    puts "Buzz"
+  elsif int%3== 0
+    puts "Fizz"
+  #else
+    #puts "Nil"
+  end
+end
+
+fizz_buzz(3)
+fizz_buzz(5)
+fizz_buzz(15)
